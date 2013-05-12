7a7f81366f1493435941d0cfc1b6423a15dfd8ec
Sean Ren <shawiz@sneakpeeq.com>
2013-05-11 22:27:28 -0700

    auto change google glass playground text

--- wenhang-dev/chrome-extension/hack.js
+++ wenhang-dev/chrome-extension/hack.js
@@ -1,22 +1,21 @@
-var changeTextArea = function(mytext) {
-	debugger;
-	var iframe = document.getElementsByTagName("iframe")[0];
-	var frameDoc = iframe.contentDocument || iframe.contentWindow.document;
-	// document.getElementById('source-html').value = mytext;
-	frameDoc.getElementById('source-json').value = mytext;
-}
+$(document).ready(function() {
 
+	function changeTextArea(message) {
+		var frame1 = innerFrame(window.document, 1);
+		var frame2 = innerFrame(frame1.document, 0);
+		var node = $(frame2.document.getElementById('map-text-node'));
+		node.find('div').text(message);
+		node.focus().blur();
+	}
 
-var pollingCheckAndSeek = function(){
-	// debugger;
-	// $("document").live("Window.mo",function(){
-	// 	addIconII(this);
-	// });
+	function innerFrame(doc, index) {
+		var iframe = doc.getElementsByTagName("iframe")[index];
+		return (iframe.contentWindow) ? iframe.contentWindow : 
+			(iframe.contentDocument.document) ? iframe.contentDocument.document : iframe.contentDocument;
+	}
 
-	changeTextArea("Hello World");
-}
-setTimeout(function(){
-	//main
-    pollingCheckAndSeek();
-}, 5000);
-
+	changeTextArea("Hello World Playground lets you experiment with how content is displayed\
+on Glass. For more information on how to setup and use the Playground, see Playground\
+Usage. To authorize the Playground to send and receive data from your account, enter\
+your Google");
+});
