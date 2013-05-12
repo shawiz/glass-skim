00f9acc9cca5d83da80cd1761caddf4bdbb61e22
Sean Ren <shawiz@sneakpeeq.com>
2013-05-12 01:13:38 -0700

    added pubsub

--- server/mushroom/__init__.py
+++ server/mushroom/__init__.py
@@ -1,6 +1,8 @@
+from redis import StrictRedis
 from flask import Flask
 from mushroom.config import settings
 
 app = Flask(__name__)
+redis = StrictRedis()
 
 import mushroom.controllers
--- server/mushroom/controllers.py
+++ server/mushroom/controllers.py
@@ -1,22 +1,52 @@
-from flask import render_template, request, session, redirect, Response
-
-from mushroom import app
-from mushroom.utils.jsonify import jsoned
-
-@app.route("/")
-@jsoned
-def get():
-    return render_template('index.html')
-
-
-@app.route("/test")
-def get():
-    return render_template('index.html')
-
+from flask import render_template, request, redirect, Response
+from mushroom import app, redis
 
 @app.route('/postText', methods=['POST'])
-def post():
-    print "roger that"
+def postText():
+    print "wait..."
     text = request.form.get('imageData')
     print text
+    redis.publish('notifications', "Hello!")
+    print "published"
     return ""
+
+
+def event_stream():
+    pubsub = redis.pubsub()
+    pubsub.subscribe('notifications')
+    for message in pubsub.listen():
+        print message
+        yield 'data: %s\n\n' % message['data']
+ 
+
+@app.route('/post', methods=['POST'])
+def post():
+    redis.publish('notifications', 'Hello!')
+    return redirect('/')
+ 
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+
+@app.route('/')
+def index():
+    return '''
+<html>
+<head>
+    <script>
+        var source = new EventSource('/stream');
+        source.onmessage = function (event) {
+             console.log(event.data);
+        };
+    </script>
+</head>
+<body>
+    <form method="POST" action="/post">
+        <input type="submit"/>
+    </form>
+</body>
+</html>
+ 
+'''
